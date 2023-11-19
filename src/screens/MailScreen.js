9f33f5b4d817e1bdb31352adb26fdf5e689d2578
Feiyang <128330965+Feiyang0303@users.noreply.github.com>
2023-11-19 00:32:37 -0500

    Update MailScreen.js

--- src/screens/MailScreen.js
+++ src/screens/MailScreen.js
@@ -1,52 +1,55 @@
+import { Notifications } from 'expo';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'; // Import FlatList
+import React, { useEffect } from 'react';
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import colors from '../../assets/colors/colors';
 import MailSearchBar from '../components/MailSearchBar';
-import { usePushNotification } from '../services/PushNotification';
-
-import messaging from '@react-native-firebase/messaging';
-import { useEffect } from 'react';
-
-
-    useEffect(() => {
-        const unsubscribe = messaging().onMessage(async remoteMessage => {
-        console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
-    
-          // Handle the incoming message, e.g., display a notification or update state
-        });
-    
-        return unsubscribe;
-    }, []);
+import { usePushNotification } from '../services/pushnotification';
 
 const MailScreen = ({ navigation }) => {
-    usePushNotification();
+usePushNotification();
 
-  // Sample messages data
+useEffect(() => {
+    const registerForPushNotifications = async () => {
+      // Check if the app has permission to receive push notifications
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+
+      // If permission has not been granted, ask for it
+    if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+    }
+   // If permission is still not granted, exit
+if (finalStatus !== 'granted') {
+    return;
+}
+
+  // Get the device's push token
+const tokenData = await Notifications.getExpoPushTokenAsync();
+const token = tokenData.data;
+
+console.log('Expo Push Token:', token);
+};
+
+// Immediately Invoked Function Expression (IIFE) to handle async code
+(async () => {
+await registerForPushNotifications();
+})();
+}, []);
+
+// Sample messages data
 const messages = [
-    { id: 1, text: 'Message 1' },
-    { id: 2, text: 'Message 2' },
-    { id: 3, text: 'Message 3' },
+{ id: 1, text: 'Message 1' },
+{ id: 2, text: 'Message 2' },
+{ id: 3, text: 'Message 3' },
 ];
-
-  // Function to navigate to IndividualMailScreen with message data
 const navigateToIndividualMail = (message) => {
     navigation.navigate('IndividualMail', { message });
 };
+
 return (
     <View>
-    <Text style={styles.header}>Mail</Text>
-    <FlatList
-        data={messages}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-        <TouchableOpacity onPress={() => navigateToIndividualMail(item)}>
-            <View style={styles.messageItem}>
-            <Text>{item.text}</Text>
-            </View>
-        </TouchableOpacity>
-        )}
-    />
     <TouchableOpacity onPress={() => navigation.navigate('AboutUs')}>
         <Image
         source={require('../../assets/adaptive-icon-cropped.png')}
@@ -60,6 +63,17 @@ return (
     </TouchableOpacity>
     <Text style={styles.header}>Mail</Text>
     <MailSearchBar />
+    <FlatList
+        data={messages}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+        <TouchableOpacity onPress={() => navigateToIndividualMail(item)}>
+            <View style={styles.messageItem}>
+        <Text>{item.text}</Text>
+            </View>
+        </TouchableOpacity>
+        )}
+    />
     <StatusBar style="auto" />
     </View>
 );
