7ff06ce630d48b35ef6719f6591afa1d2a43e25e
Feiyang <128330965+Feiyang0303@users.noreply.github.com>
2023-11-19 00:32:58 -0500

    Update PushNotification.js

--- src/services/PushNotification.js
+++ src/services/PushNotification.js
@@ -1,42 +1,45 @@
-// src/services/PushNotification.js
-
-import messaging from '@react-native-firebase/messaging';
-import { initializeApp } from 'firebase/app';
+import Constants from 'expo-constants'; // Import Constants from 'expo-constants'
+import * as Notifications from 'expo-notifications';
 import { useEffect } from 'react';
-import PushNotification from 'react-native-push-notification';
-
-// Initialize Firebase
-const firebaseConfig = {
-  // ... your config
-};
-
-const app = initializeApp(firebaseConfig);
 
 const usePushNotification = () => {
-useEffect(() => {
-    const requestUserPermission = async () => {
-    const authStatus = await messaging().requestPermission();
-    const enabled =
-        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  useEffect(() => {
+    const registerForPushNotifications = async () => {
+      try {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
 
-    if (enabled) {
-        console.log('Authorization status:', authStatus);
+        if (existingStatus !== 'granted') {
+          const { status } = await Notifications.requestPermissionsAsync();
+          if (status !== 'granted') {
+            console.log('Failed to get push token for push notification!');
+            return;
+          }
+        }
 
-        // Get the FCM token
-        const fcmToken = await messaging().getToken();
-        console.log('FCM Token:', fcmToken);
+        // Retrieve projectId from Constants.expoConfig
+        const projectId = Constants.expoConfig.extra.projectId;
 
-        // Send a test notification
-        PushNotification.localNotification({
-        title: 'Test Notification',
-        message: 'This is a test notification.',
+        const expoPushToken = await Notifications.getExpoPushTokenAsync({
+          projectId: '00ab2240-c3a4-4974-87cf-2d41f65b9b0e' // Replace with your actual project ID
         });
-    }
+        const token = expoPushToken.data;
+        console.log('Expo Push Token:', token);
+
+        // Example: send a test notification
+        await Notifications.scheduleNotificationAsync({
+          content: {
+            title: 'Test Notification',
+            body: 'This is a test notification.',
+          },
+          trigger: null, // Send immediately
+        });
+      } catch (error) {
+        console.error('Error getting or scheduling push notification:', error);
+      }
     };
 
-    requestUserPermission();
-}, []);
+    registerForPushNotifications();
+  }, []);
 };
 
-export { app, usePushNotification };
+export { usePushNotification };
